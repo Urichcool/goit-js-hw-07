e776dd2d2fff8f162977a5b6bd4ed030a9028425
Urichcool <104870932+Urichcool@users.noreply.github.com>
2022-09-11 01:12:42 +0200

    render-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const addImagesMarkup = galleryItems
+  .map(
+    (img) => `<div class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", addImagesMarkup);
+const onGalleryLinkClick = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImg = event.target.dataset.source;
+  console.log(selectedImg);
+  return selectedImg;
+  
+}
+galleryEl.addEventListener("click", onGalleryLinkClick);
+
 
 console.log(galleryItems);
