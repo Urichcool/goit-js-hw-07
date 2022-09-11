c5b443a96d46790a283f07d8d4fb77534492044e
Urichcool <urichcoolx@gmail.com>
2022-09-11 18:03:24 +0200

    done task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const galleryList = document.querySelector(".gallery");
+const addImagesMarkup = galleryItems
+  .map((img) => 
+    `<li><a class="gallery__item" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+      title = "${img.description}"
+    />
+  </a></li>`
+  )
+  .join("");
+galleryList.insertAdjacentHTML("beforeend", addImagesMarkup);
+new SimpleLightbox(".gallery a", {captionDelay : 250});
 console.log(galleryItems);
